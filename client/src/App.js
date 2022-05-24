8dea69a7f71d90d9f3555128b4678496a8fc0fcd
Saurabh Dabas <developerdabas@gmail.com>
2022-05-24 18:14:42 +0530

    setup Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import Login from './components/Buttons/Login';
+import Redirect from './components/Redirect';
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Grid 
+      sx={{
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: '#0C0906'
+    }}
+    display='flex'
+    flexDirection={'column'}>
+      <Routes> 
+        <Route path="/" 
+          element={
+            <Login/>
+          }
+        />
+        <Route path="/redirect" 
+          element={
+            <Redirect/>
+          }
+        />  
+      </Routes>
+    </Grid>
   );
 }
 
