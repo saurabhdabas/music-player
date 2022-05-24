57da592e22f0e731072a96b4ced7bd8c0095a51c
Saurabh Dabas <developerdabas@gmail.com>
2022-05-24 18:57:46 +0530

    Handle login from spotify account

--- client/src/components/Buttons/Login.js
+++ client/src/components/Buttons/Login.js
@@ -5,13 +5,11 @@ const Login = () => {
   const {
     REACT_APP_CLIENT_ID,
     REACT_APP_AUTHORIZE_URL,
-    REACT_APP_REDIRECT_URL,
-    HOST,
-    KEY
+    REACT_APP_REDIRECT_URL
   } = process.env;
   console.log("env:",process.env);
   const handleLogin = () => {
-    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    window.location.href = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
   };
 
   return (
